17d7c6c381ff7a956ee6c9a61e3fb261e82cce47
Ronaldo <ronaldoandrade966@gmail.com>
2022-08-25 10:28:16 -0300

    express working

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-testeeeeee
+import express from "express";
+
+const server = express()
+
+server.get("/", (req, res) => {
+    res.send("FOIIIIIII")
+})
+
+server.listen(5000, () => {console.log("foiiii")})
