d62a758f61b57e021a7bf12bf67185cc7e4073a3
Ronaldo <ronaldoandrade966@gmail.com>
2022-08-26 11:13:38 -0300

    sign-up and tweet post, tweets get

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 import express from "express";
+import cors from "cors";
 
-const server = express()
+const server = express();
 
-server.get("/", (req, res) => {
-    res.send("FOIIIIIII")
+server.use(express.json());
+server.use(cors());
+
+let users = [];
+let avatar;
+
+const tweets = [];
+
+server.post("/sign-up", (req, res) => {
+    users.push(req.body);
+    avatar = req.body.avatar
+    res.send("OK");
+})
+
+server.post("/tweets", (req, res) => {
+    if (tweets.length === 10) {
+        tweets.splice(0, 1);
+    }
+    req.body.avatar = avatar;
+    tweets.push(req.body);
+    res.send("OK")
+})
+
+server.get("/tweets", (req, res) => {
+   res.send(tweets)
 })
 
 server.listen(5000, () => {console.log("foiiii")})
